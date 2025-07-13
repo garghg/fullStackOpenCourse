215faf345e7237ce2bfbe2cc6664578e36bef70b
Haardik Garg <thehaardikgarg@gmail.com>
2025-07-13 14:45:38 -0700

    ex1-7 complete

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,20 +8,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [scores, setScores] = useState([])
+  const dataNum = scores.length
+  const dataTotal = scores.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
 
   const goodClick = () =>{
     const updateGood = good + 1
     setGood(updateGood)
+    setTotal(newtotal => newtotal + 1 )
+    setScores(prevScores => prevScores.concat(1))
   }
 
   const badClick = () =>{
     const updateBad = bad + 1
     setBad(updateBad)
+    setTotal(newtotal => newtotal + 1 )
+    setScores(prevScores => prevScores.concat(-1))
   }
 
   const neutralClick = () =>{
     const updateNeutral = neutral + 1
     setNeutral(updateNeutral)
+    setTotal(newtotal => newtotal + 1 )
+    setScores(prevScores => prevScores.concat(0))
   }
 
 
@@ -39,6 +49,12 @@ const App = () => {
       Neutral Counter: {neutral}
       <br />
       Bad Counter: {bad}
+      <br />
+      Total Feedback: {total}
+      <br />
+      Average Score: {isNaN(dataTotal / dataNum) ? 'Not Enough Data' : (dataTotal / dataNum)}
+      <br />
+      {isNaN((good/dataNum)*100) ? '': `${(good/dataNum)*100}% of Customers Liked Unicafe`}
     </div>
   )
 }
