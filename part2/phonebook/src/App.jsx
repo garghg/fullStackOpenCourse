a81841d94871ee5166e83390695a132518c577a5
Haardik Garg <hghaardik@gmail.com>
2026-01-23 10:57:15 -0500

    completed ex-2.16-2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,12 +3,14 @@ import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonsForm from './components/PersonsForm'
 import numberServices from './services/number'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchVal, setSearchVal] = useState('')
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     numberServices
@@ -20,8 +22,22 @@ const App = () => {
 
   const updateContact = (target) => {
     if (window.confirm(`${newName} is already in the phonebook. Update number?`)) {
-      numberServices.update(target.id, {...target, number: newNumber}).then(returnedPersons=> {
+      numberServices.update(target.id, {...target, number: newNumber})
+      .then(returnedPersons=> {
         setPersons(persons.map(person => person.id === target.id ? returnedPersons : person))
+        setMessage(`Updated ${newName}'s number`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 3000)
+      })
+      .catch(error => {
+        setMessage(`${newName}'s information was not found in the server.`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 3000)
+        numberServices.getAll().then(response => setPersons(response))
+        setNewName('')
+        setNewNumber('')
       })
     }
   }
@@ -42,6 +58,10 @@ const App = () => {
         .create(newNumObj)
         .then(returnedNum => {
           setPersons(persons.concat(returnedNum))    
+          setMessage(`Added ${newName}`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 3000)
           setNewName('')
           setNewNumber('')
         })
@@ -81,6 +101,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter searchVal={searchVal} handleSearch={handleSearch}/>
       <h2>Add a Contact</h2>
       <PersonsForm addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
