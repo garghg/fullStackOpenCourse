2cc120f640b7e234b855427a641c5f176e38eee8
Haardik Garg <hghaardik@gmail.com>
2026-01-23 07:41:32 -0500

    completed ex-2.14

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,15 +1,19 @@
-const Person = ({ name, number }) => {
+const Button = ({ name, id, handleDel }) => <button onClick={() => handleDel(name, id)}>Delete Contact</button>
+
+const Person = ({ name, number, id, handleDel }) => {
   return (
     <div>
     {name + " " + number}
+    {<Button name={name} id={id} handleDel={handleDel}/>}
     </div>
+    
   )
 }
 
-const Persons = ({ arr }) => {
+const Persons = ({ arr, handleDel }) => {
   return (
     <>
-      {arr.map(person => <Person name={person.name} key={person.name} number={person.number}/>)}
+      {arr.map(person => <Person name={person.name} key={person.name} number={person.number} id={person.id} handleDel={handleDel}/>)}
     </>
   )
 }
