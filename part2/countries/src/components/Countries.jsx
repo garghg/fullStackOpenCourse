ffa4813c67794d20536d8aad68193e2d9640f43b
Haardik Garg <hghaardik@gmail.com>
2026-01-31 07:33:34 -0500

    completed ex-2.19

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,12 +1,38 @@
-const CountryList = ({ country }) => {
-    return (
+import { useState } from "react"
+
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>
+
+const CountryList = ({ country, countriesData }) => {
+    const [show, setShow] = useState(false)
+
+    const handleShow = () => {
+        const updatedShow = !show
+        setShow(updatedShow)
+        return updatedShow
+    }
+
+    if (show) {
+        return (
+            <div>
+                <ShowCountry countriesData={countriesData} selectedCountry={country}/>
+            </div>
+        )
+    } else {
+        return (
         <div>
-            <p>{country}</p>
+            <p>{country}<Button text={'Show'} onClick={handleShow}/></p>
         </div>
-    )
+        )
+    }
 }
 
-const Country = ({ name, flag, capital, area, languages }) => {
+const ShowCountry = ({ countriesData, selectedCountry }) => {
+    const countryData = countriesData.filter(country => country.name.common === selectedCountry)[0]
+    const flag = countryData.flags.png
+    const name = countryData.name.common
+    const capital = countryData.capital[0]
+    const area = countryData.area
+    const languagesArr = Object.values(countryData.languages)
     return (
         <div>
             <h1>{name}</h1>
@@ -14,7 +40,7 @@ const Country = ({ name, flag, capital, area, languages }) => {
             <p>Area: {area}</p>
             <h2>Languages</h2>
             <ul>
-                {languages.map(language => (<li>{language}</li>))}
+                {languagesArr.map(language => (<li key={language}>{language}</li>))}
             </ul>
             <img src={flag} alt="Flag" />
         </div>
@@ -23,28 +49,17 @@ const Country = ({ name, flag, capital, area, languages }) => {
 
 
 const Countries = ({ countries, countriesData }) => {
+
     if (countries.length >= 10) {
         return (
             <div>
                 Too many countries; Please be more specific.
             </div>
         )
-    } else if (countries.length === 1) {
-        const country = countriesData.filter(country => country.name.common === countries[0])[0]
-        const flag = country.flags.png
-        const name = country.name.common
-        const capital = country.capital[0]
-        const area = country.area
-        const languagesArr = Object.values(country.languages)
-        return (
-            <div>
-                <Country name={name} flag={flag} capital={capital} area={area} languages={languagesArr}/>
-            </div>
-        )
     } else {
         return (
         <div>
-            {countries.map(country => <CountryList country={country} key={country}/>)}
+            {countries.map(country => <CountryList country={country} key={country} countriesData={countriesData}/>)}
         </div>
     )
     }
