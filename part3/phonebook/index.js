1c17d16bf9cde9ecc05d5339e0d99869f52f38b3
Haardik Garg <hghaardik@gmail.com>
2026-02-05 19:36:36 -0500

    completed ex-3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -40,6 +40,17 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${total_contacts} people<br>${now}`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const contact = contacts.find(contact => contact.id === id)
+
+    if (contact) {
+        response.json(contact)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
