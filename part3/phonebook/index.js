71a65d28c1a0c6a2b4eee176340e67b746b415ea
Haardik Garg <hghaardik@gmail.com>
2026-02-15 08:04:22 -0500

    databases ex-3.13-14 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
+const Contact = require('./models/contact')
+
+const app = express()
 
 app.use(express.static('dist'))
 app.use(express.json())
-
 
 morgan.token('content', function getContent(request, response) {
     if (request.method === 'POST') {
@@ -16,35 +18,16 @@ morgan.token('content', function getContent(request, response) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 
-let contacts = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let contacts = []
 
 app.get('/', (request, response) => {
     response.send('Test')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(contacts)
+    Contact.find({}).then((notes) => {
+      response.json(notes)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -54,14 +37,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const contact = contacts.find(contact => contact.id === id)
-
-    if (contact) {
+    Contact.findbyID(request.params.id).then(contact => {
         response.json(contact)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -70,10 +48,6 @@ app.delete('/api/persons/:id', (request, response) => {
 
     response.status(204).end()
 })
-
-const generateId = () => {
-    return String(Math.floor(Math.random() * 999999) + 1000)
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -90,25 +64,17 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const duplicate = contacts.find(contact => contact.name === body.name)
-
-    if (duplicate) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const contact = {
-        id: generateId(),
+    const contact = new Contact({
         name: body.name,
         number: body.number
-    }
+    })
 
-    contacts = contacts.concat(contact)
-    response.json(contact)
+    contact.save().then((savedContact) => {
+        response.json(savedContact)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
