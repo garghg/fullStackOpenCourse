d6e1773cd2bac1a221531f96b69fff13be865cd8
Haardik Garg <hghaardik@gmail.com>
2026-02-15 06:22:00 -0500

    databases ex-3.12 completed

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
 
-const uri=process.env.URI
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const uri = `mongodb+srv://hghaardik_db_user:${password}@cluster0.snktaat.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.connect(uri, { family: 4 })
 
@@ -12,12 +18,34 @@ const contactSchema = new mongoose.Schema({
 
 const Contact = mongoose.model('Contact', contactSchema)
 
-const contact = new Contact({
-  name: "Mary Poppendieck", 
-  number: "39-23-6423122"
-})
 
-contact.save().then(result => {
-  console.log('contact saved!')
+if (process.argv.length === 3) {
+  
+  Contact.find({}).then(result => {
+    result.forEach(contact => {
+      console.log(contact)
+    })
+    mongoose.connection.close()
+  })
+
+} else if (process.argv.length === 5) {
+
+  const new_name = process.argv[3]
+  const new_number = process.argv[4]
+
+  const contact = new Contact({
+    name: new_name,
+    number: new_number
+  })
+
+  contact.save().then(result => {
+    console.log(`Added ${new_name} ${new_number} to phonebook`)
+    mongoose.connection.close()
+  })
+
+} else {
+  console.log("Usage:")
+  console.log("  node script.js <password>                    # list contacts")
+  console.log("  node script.js <password> <name> <number>    # add contact")
   mongoose.connection.close()
-})
+}
