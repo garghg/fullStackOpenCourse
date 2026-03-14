4738be4ccd5e2d4ce4b596934cd36f3feb9e6ee6
Haardik Garg <hghaardik@gmail.com>
2026-03-13 20:37:04 -0400

    started ex-4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,18 @@
+const Blog = require('../models/blog')
+
 const dummy = (blogs) => {
   return 1
 }
 
-module.exports = {
-  dummy
+
+// ***********************
+// fix this
+// ***********************
+
+const totalLikes = () => {
+  return Blog.find({}).then((blogs) => {
+    return blogs.reduce((total, blog) => total += blog.likes, 0)
+  })
 }
+
+module.exports = { dummy, totalLikes }
