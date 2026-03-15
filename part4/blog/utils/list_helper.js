db6e6ad686b62fa64839ecfff5930f49d23ef57a
Haardik Garg <hghaardik@gmail.com>
2026-03-14 20:28:26 -0400

    finished ex-4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -4,15 +4,8 @@ const dummy = (blogs) => {
   return 1
 }
 
-
-// ***********************
-// fix this
-// ***********************
-
-const totalLikes = () => {
-  return Blog.find({}).then((blogs) => {
-    return blogs.reduce((total, blog) => total += blog.likes, 0)
-  })
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
 module.exports = { dummy, totalLikes }
