76cec845470cedd7f8fc05a624c9a12ab117dc2b
Haardik Garg <hghaardik@gmail.com>
2026-05-11 15:16:13 -0700

    lesson code and ex-4.8

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,7 +5,7 @@ const blogRouter = require('./controllers/blogs')
 
 const app = express()
 
-mongoose.connect(config.mongoUrl, { family: 4 })
+mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
