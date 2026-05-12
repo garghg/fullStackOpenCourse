744f34a0cd3df936544187e246f7679d7a153ad2
Haardik Garg <hghaardik@gmail.com>
2026-05-12 16:27:55 -0700

    finished ex-4.15

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 
 const app = express()
 
@@ -9,5 +10,6 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 module.exports = app
