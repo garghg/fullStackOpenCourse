1e68daad7116c3903ad49016b7fce7529c61633f
Haardik Garg <hghaardik@gmail.com>
2026-05-12 17:06:12 -0700

    started ex-4.16

---
+++ part4/blog/utils/middleware.js
@@ -0,0 +1,17 @@
+const logger = require('./logger')
+
+const errorHandler = (error, request, response, next) => {
+    logger.error(error.message)
+
+    if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: 'invalid username' })
+    } else if (error.name == 'PasswordError') {
+        return response.status(400).send({ error: error.message })
+    } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+        return response.status(400).json({ error: 'expected `username` to be unique' })
+    } 
+
+    next(error)
+}
+
+module.exports = { errorHandler }
