abf5561dd7f9e4de0c4ca4ef1c69c85fd349a9b0
Haardik Garg <hghaardik@gmail.com>
2026-05-13 10:48:53 -0700

    finished ex-4.16

---
+++ part4/blog/tests/user_api.test.js
@@ -0,0 +1,63 @@
+const { test, after, beforeEach, before, describe } = require('node:test')
+const assert = require('node:assert')
+const helper = require('./test_helper')
+const mongoose = require('mongoose')
+const User = require('../models/user')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+describe('invalid input', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+        await User.insertMany(helper.initialUsers)
+    })
+
+    const invalid_check = async (newUser) => {
+        const post_response = await api
+                        .post('/api/users')
+                        .send(newUser)
+        
+        assert.strictEqual(post_response.status, 400)
+
+        const get_response = await api.get('/api/users')
+        assert.strictEqual(get_response.body.length, helper.initialUsers.length)
+    }
+
+    test('invalid username', async () => {
+        const newUser = {
+            username: 'bs',
+            name: 'Bob Smith',
+            password: 'bob123'
+        }
+
+        await invalid_check(newUser)
+
+    })
+
+    test('invalid password', async () => {
+        const newUser = {
+            username: 'bobsmith',
+            name: 'Bob Smith',
+            password: 'bs'
+        }
+
+        await invalid_check(newUser)
+    })
+
+    test('duplicate username', async () => {
+        const newUser = {
+            username: 'janedoe',
+            name: 'Jane Doe',
+            password: 'jane123'
+        }
+
+        await invalid_check(newUser)
+    })
+
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
