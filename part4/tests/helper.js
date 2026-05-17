d6d31d0467408407fa6fcc39cc77cbee00e61ddb
Haardik Garg <hghaardik@gmail.com>
2026-05-17 13:43:36 -0700

    finished ex-5.20

--- part4/tests/helper.js
+++ part4/tests/helper.js
@@ -4,4 +4,15 @@ const loginHelper = async (page, username, password) => {
     await page.getByRole('button', { name: 'Login' }).click()
 }
 
-export { loginHelper }
+const addInitialBlog = async page => {
+    await page.getByRole('button', { name: 'Create new blog' }).click()
+    await page.getByPlaceholder('Enter Blog Title')
+        .fill('Adding blog from testing')
+    await page.getByPlaceholder('Enter Blog URL')
+        .fill('www.example.com')
+    await page.getByPlaceholder('Enter Blog Author')
+        .fill('Haardik Garg')
+    await page.getByRole('button', { name: 'Add Blog' }).click()
+}
+
+export { loginHelper, addInitialBlog }
