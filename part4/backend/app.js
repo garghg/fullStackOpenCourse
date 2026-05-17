f26ed2ffd5cb86e5b761345f69fb2691f6acf962
Haardik Garg <hghaardik@gmail.com>
2026-05-17 09:24:33 -0700

    finished ex-5.18

--- part4/backend/app.js
+++ part4/backend/app.js
@@ -15,6 +15,10 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+if (process.env.NODE_ENV === 'test') {
+    const testRouter = require('./controllers/testing')
+    app.use('/api/testing', testRouter)
+}
 app.use(middleware.errorHandler)
 
 module.exports = app
