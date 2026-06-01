a8109dfab50a7dfb03dc454853370b2034c38bff
Haardik Garg <hghaardik@gmail.com>
2026-06-01 14:32:49 -0700

    finished ex-8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -95,7 +95,7 @@ const typeDefs = /* GraphQL */ `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -104,7 +104,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => args.author ? books.filter(b => b.author === args.author) : books,
     allAuthors: () => {
       return authors.map((a) => ({
         name: a.name,
